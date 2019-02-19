1c935394d0637a17a860ca3666e2c8c0ef719310
user3228 <st048497@student.spbu.ru>
2019-02-19 10:20:16 +0300

    Update printf_test.cpp

--- printf/src/test/printf/printf_test.cpp
+++ printf/src/test/printf/printf_test.cpp
@@ -16,17 +16,5 @@ TEST(message_function_test, right_result) {
 
   std::string right_result = "a + 2 = 3";
   EXPECT_EQ(right_result, result);
-}#include <iostream>
-#include <string>
-#include <sstream>
-#include <vector>
-#include <utility>
-#include <gtest/gtest.h>
-#include <printf/printf.h>
-Test(printf,Compare){
-stingstream ss;
-ss<<message(cout, "% + % = %\n",'a',2)
-string k = ss.str();
-EXPECT_EQ("a + 2 = %",k);
 }
 
